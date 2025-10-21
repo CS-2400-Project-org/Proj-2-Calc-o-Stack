109b914adf63de10849293698ccc9ed689c8c64c
Kayshawnwilliams2@gmail.com <kayshawnw@cpp.edu>
2025-10-21 16:34:43 -0700

    Added resizable array class and evaluate postfix methods

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,52 @@
+import java.lang.Character;
 public class Calculator {
     
+
+
+
+    public Integer evaluatePostfix(String postFix){
+    
+        ResizableArrayStack<Integer> valueStack = new ResizableArrayStack<>(postFix.length());
+        int index = 0;
+        while(index < postFix.length()){
+            char nextCharacter = postFix.charAt(index);
+            if(Character.isDigit(nextCharacter)){
+
+                valueStack.push(Integer.valueOf(nextCharacter));
+            }
+            else if(nextCharacter == '+' ||nextCharacter == '-'||nextCharacter == '*'
+                    || nextCharacter == '/' || nextCharacter == '^'){
+
+                Integer operandTwo = valueStack.pop();
+                Integer operandOne = valueStack.pop();
+
+                Integer result = operate(operandOne, operandTwo, nextCharacter);
+                valueStack.push(result);
+            }
+            index++;
+        }
+
+        return valueStack.peek();
+    }
+
+    public Integer operate(Integer operand1, Integer operand2, char operation){
+        if(operation == '+'){
+            return operand1 + operand2;
+
+        } else if (operation == '-') {
+            return operand1 - operand2;
+
+        } else if (operation == '*') {
+            return operand1 * operand2;
+
+        } else if (operation == '/') {
+            return operand1 / operand2;
+
+        } else if (operation == '^') {
+            return operand1^operand2;
+
+        } 
+
+        return Integer.valueOf(999999);
+    }
 }
--- ResizableArrayStack.java
+++ ResizableArrayStack.java
@@ -1,3 +1,88 @@
-public class ResizableArrayStack implements StackInterface {
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class ResizableArrayStack<T> implements StackInterface<T> {
+    private T[] stack;
+    private int topIndex;
+    private final static int DEFAULT_CAPACITY = 50;
+    private final static int MAX_CAPACITY = 10000;
+    private boolean integrityOk;
     
+    
+    public ResizableArrayStack(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public ResizableArrayStack(int initialCapacity){
+        integrityOk = false;
+        checkCapacity(initialCapacity);
+
+        @SuppressWarnings("unchecked")
+        T[] tempStack = (T[])new Object[initialCapacity];
+        stack=tempStack;
+        topIndex=-1;
+        integrityOk = true;
+    }
+
+    public void push(T newEntry){
+        checkIntegrity();
+        ensureCapacity();
+
+        stack[++topIndex] = newEntry;
+    }
+
+    public T pop(){
+        checkIntegrity();
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        T result = stack[topIndex];
+        stack[topIndex--] = null;
+        return result;
+
+    }
+
+    public T peek(){
+        checkIntegrity();
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stack[topIndex];
+
+    }
+
+    public void clear(){
+        checkIntegrity();
+
+        while (topIndex > -1){
+            stack[topIndex--] = null;
+        }
+    }
+
+
+    public boolean isEmpty(){
+        if(topIndex == -1) return true;
+        return false;
+
+    }
+
+    private void checkIntegrity(){
+        if(!integrityOk) throw(new SecurityException("Stack is corrupt and cannot be used."));
+    }
+
+    private void checkCapacity(int checkVal){
+        if(checkVal>MAX_CAPACITY){
+            throw new IllegalStateException("Attempt to create a stack whose"+
+                                            " capacity exceeds maximul allotment of " +
+                                            MAX_CAPACITY);
+        }
+    }
+    private void ensureCapacity(){
+        if(topIndex==stack.length-1){
+            int newLength = stack.length * 2;
+            checkCapacity(newLength);
+            stack = Arrays.copyOf(stack, newLength);
+        }
+    }
 }
