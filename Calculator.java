965504079a4fcab61f3ef5e495e22ce17e90deef
Jahnvi <jlal@cpp.edu>
2025-10-25 20:24:54 -0700

    Made Additions to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,98 @@
 import java.lang.Character;
-public class Calculator {
-    
 
-    public Integer convertToPostfix(String infix)
+public class Calculator 
+{
+    public String convertToPostfix(String infix)
     {
+        StackInterface<Character> operatorStack = new LinkedStack<>();
+        StringBuilder postfix = new StringBuilder();
+        char topOperator;
+        int index = 0;
+
+        while(index < infix.length())
+        {
+            char nextChar = infix.charAt(index);
+
+            //Ignore whitespace
+            if(Character.isWhitespace(nextChar))
+            {
+                index++;
+                continue;
+            }
+
+            switch (nextChar)
+            {
+                // Case 1: Operand
+                default: 
+                if(Character.isLetterOrDigit(nextChar))
+                {
+                    postfix.append(nextChar);
+                }
+                break;
+
+                // Case 2: Exponent (right-associative)
+                case '^' :
+                while (!operatorStack.isEmpty() &&  
+                precedence(nextChar) < precedence(operatorStack.peek()))
+                {
+                    postfix.append(operatorStack.pop());
+                }
+                operatorStack.push(nextChar);
+                break; 
+
+                // Case 3: Operators
+                case '+' : case '-' : case '*' : case '/' : 
+                while (!operatorStack.isEmpty() &&  
+                precedence(nextChar) <= precedence(operatorStack.peek()))
+                {
+                    postfix.append(operatorStack.pop());
+                }
+                operatorStack.push(nextChar);
+                break;
+
+                // Case 4: Left Parenthesis
+                case '(' :
+                operatorStack.push(nextChar);
+                break;
+
+                // Case 5: Right Parenthesis
+                case ')' : // Stack is not empty if infix expression is valid 
+                topOperator = operatorStack.pop();
+                while (topOperator != '(')
+                {
+                    postfix.append(topOperator);
+                    topOperator = operatorStack.pop();
+                }
+                break;
+            }
+
+            index++;
+        }
+        while (!operatorStack.isEmpty())
+        {
+            topOperator = operatorStack.pop();
+            postfix.append(topOperator);
+        }
+
+        return postfix.toString();       
     }
 
+    private static int precedence(char operator)
+    {
+        switch(operator)
+        {
+            case '^':
+            return 3;
+            case '*':
+            case '/':
+            return 2;
+            case '+':
+            case '-':
+            return 1;
+            default: // For non-operators
+            return 0;
+        }
+    }
 
     public Integer evaluatePostfix(String postFix){
     
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,3 +1,4 @@
-public class CalculatorTest {
+public class CalculatorTest 
+{
     
 }
